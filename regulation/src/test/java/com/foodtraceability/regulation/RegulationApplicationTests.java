6a1b285449544e0aa5b848b3b3eb9d753f597485
HZP3234 <2318141726@qq.com>
2026-06-23 14:21:52 +0800

    架构

---
+++ regulation/src/test/java/com/foodtraceability/regulation/RegulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodtraceability.regulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
